1b3a4c988d16f7be654f766c82eb6be7c3f80880
Sue-52 <1219243947@qq.com>
2022-05-03 14:47:37 +0800

    submit：Map 案例

--- 03.TypeScript cases/01.Map/src/Map.ts
+++ 03.TypeScript cases/01.Map/src/Map.ts
@@ -1,3 +1,6 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
 export class Map {
   // 存储地图实例
   private googleMap: google.maps.Map;
@@ -12,4 +15,48 @@ export class Map {
       }
     )
   }
+
+  // 添加地图标记
+  // // 在地图中标记用户位置
+  // addUserMarker(user: User) {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: user.location.lat,
+  //       lng: user.location.lng
+  //     },
+  //   })
+  // }
+  // // 在地图中标记公司位置
+  // addCompanyMarker(company: Company) {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     },
+  //   })
+  // }
+  // 合并地图和用户位置
+
+  addMarker(mappable: User | Company) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      },
+    })
+    // 创建标记
+    const marker = new google.maps.Marker();
+    // 设置标记的位置
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hello I am marker content"
+      })
+      infoWindow.open(this.googleMap, marker)
+    })
+  }
+
+
 }
--- 03.TypeScript cases/01.Map/src/index.ts
+++ 03.TypeScript cases/01.Map/src/index.ts
@@ -1,11 +1,13 @@
+// src/index.ts
 import { User } from "./User";
 import { Company } from "./Company";
-// src/index.ts
 import { Map } from "./Map";
+
 const user = new User();
 const company = new Company();
-// 创建地图
-new Map("map");
 
-// console.log(user);
-// console.log(company);
+// 创建地图
+const map = new Map("map");
+
+map.addMarker(user);
+map.addMarker(company);
